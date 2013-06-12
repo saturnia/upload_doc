682184253d50ca30a05f2c8cfdbb35321907d7fc
Martin Gyhagen <martin.gyhagen@gmail.com>
2013-06-12 15:46:32 +0200

    Finish static home page

--- Gemfile
+++ Gemfile
@@ -1,11 +1,11 @@
 source 'https://rubygems.org'
-
+gem 'nokogiri'
 gem 'rails', '3.2.13'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'pg'
+gem 'pg', '0.12.2'
 
 group :development, :test do 
 	gem 'rspec-rails', '2.11.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,4 +1,20 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+	describe "Home page" do
+
+		# Nedan följer en rad självklara test
+		it "should have the h1 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('h1', text: 'Home')
+		end
+
+		it "should have the title 'Home'" do
+			visit '/static_pages/home'
+			page.should have_selector('title', text: 'UploadDoc | Home')
+		end
+
+
+	end
 end
